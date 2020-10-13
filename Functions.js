5b0803d8407d36313f628ca610bbfb6944cc233c
DitCesar <72680759+DitCesar@users.noreply.github.com>
2020-10-13 13:21:05 -0500

    Agregar archivos de proyecto.

---
+++ Functions.js
@@ -0,0 +1,29 @@
+/**        
+ *      Funtions
+ */
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+console.log(add(3, 5))          //8
+console.log(add(4, 7))          //11
+console.log(subtract(8, 4))     //4
+console.log(subtract(6, 4))     //2
+console.log(multiply(3, 4))     //12
+console.log(multiply(8, 9))     //72
+console.log(divide(16, 4))      //4
+console.log(divide(18, 9))      //2
+
---
+++ Functions.py
@@ -0,0 +1,27 @@
+'''       
+        Functions
+'''
+ 
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    print(add(5, 7))            #12
+    print(add(9, 15))           #24
+    print(subtract(13, 4))      #9
+    print(subtract(5, 1))       #4
+    print(multiply(11, 3))      #33
+    print(multiply(7, 9))       #63
+    print(divide(12, 4))        #3
+    print(divide(15, 3))        #5
+
+    
